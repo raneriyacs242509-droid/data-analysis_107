4f249f03e47107121975290292eb4c0a27fcf0a2
raneriyacs242509-droid <raneriyacs242509@gmail.com>
2026-02-08 12:04:45 +0530

    Update PRAC13_M2.R

--- PRAC13_M2.R
+++ PRAC13_M2.R
@@ -1,35 +1,23 @@
 
-rm(list = ls())
+wine <- read.csv("C:/Users/vibro/Downloads/wine_quality_merged.csv")
+
+head(wine)
 
 
-student_df <- read.csv("C:/Users/yukta/Downloads/StudentPerformance (1).csv", header = TRUE)
-
-
-head(student_df)
-str(student_df)
-
-
-numeric_df <- student_df[, sapply(student_df, is.numeric)]
-
-
-head(numeric_df)
-
-
-model <- lm(numeric_df[, 1] ~ numeric_df[, 2])
+model <- lm(quality ~ alcohol, data = wine)
 
 
 summary(model)
 
 
-plot(
-  numeric_df[, 2],
-  numeric_df[, 1],
-  main = "Linear Regression: Student Performance Dataset",
-  xlab = colnames(numeric_df)[2],
-  ylab = colnames(numeric_df)[1],
-  pch = 19,
-  col = "orange"
-)
+plot(wine$alcohol, wine$quality,
+     main = "Linear Regression: Alcohol vs Wine Quality",
+     xlab = "Alcohol",
+     ylab = "Quality",
+     pch = 16)
 
-abline(model, col = "blue", lwd = 2)
+# Step 6: Add regression line
+abline(model)
+
+
 
